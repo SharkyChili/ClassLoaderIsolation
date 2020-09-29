211561414676238d95a512bb0293bda0b22dc0ab
fw1036994377@outlook.com <fw142857>
2020-09-29 18:41:28 +0800

    back home

---
+++ Adriver/src/main/java/org/wayne/Driver.java
@@ -0,0 +1,76 @@
+package org.wayne;
+
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Driver implements java.sql.Driver {
+    static
+    {
+        try
+        {
+            // moved the registerDriver from the constructor to here
+            // because some clients call the driver themselves (I know, as
+            // my early jdbc work did - and that was based on other examples).
+            // Placing it here, means that the driver is registered once only.
+            java.sql.DriverManager.registerDriver(new Driver());
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        System.out.println("Adriver");
+        Properties properties = new Properties();
+        return makeConnection(url,properties);
+    }
+
+    private static Connection makeConnection(String url, Properties props) throws SQLException {
+        return new WayneConnection();
+    }
+
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        System.out.println("Adriver");
+        return false;
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        System.out.println("Adriver");
+        return new DriverPropertyInfo[0];
+    }
+
+    @Override
+    public int getMajorVersion() {
+        System.out.println("Adriver");
+        return 0;
+    }
+
+    @Override
+    public int getMinorVersion() {
+        System.out.println("Adriver");
+        return 0;
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        System.out.println("Adriver");
+        return false;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        System.out.println("Adriver");
+        return null;
+    }
+}
