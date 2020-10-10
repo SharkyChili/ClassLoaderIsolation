8595d5a30138dbf8cc87b79865125d65dc05624c
fw1036994377@outlook.com <fw142857>
2020-10-10 10:16:14 +0800

    略做修改

--- IsolationExample/APlugin/src/main/java/org/wayne/App.java
+++ IsolationExample/APlugin/src/main/java/org/wayne/App.java
@@ -1,13 +1,28 @@
 package org.wayne;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public void registerDriver(String driver) throws Exception{
+        Class.forName("org.wayne.Driver");
     }
+
+    public Connection getConnection(String url) throws Exception{
+//        Class.forName("org.wayne.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:wayne://172.19.1.49:7300/dwtmppdb");
+        return conn;
+    }
+
+    public Statement createStatement(Connection conn) throws Exception{
+        Statement statement = conn.createStatement();
+        return statement;
+    }
+
+
 }
