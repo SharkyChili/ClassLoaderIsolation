4d8f6994a67d38f84c03fab1ad3dca8f1f02f6c6
fw1036994377@outlook.com <fw1036994377@outlook.com>
2020-10-12 02:17:52 +0800

    做一些工程化

--- IsolationExample/IsolationApi/src/main/java/org/wayne/util/ClassLoaderUtil.java
+++ IsolationExample/IsolationApi/src/main/java/org/wayne/util/ClassLoaderUtil.java
@@ -1,6 +1,8 @@
 package org.wayne.util;
 
 
+import org.wayne.classloader.ClassLoaderFactory;
+import org.wayne.classloader.SelfDefinedClassLoader;
 import org.wayne.enums.EnvEnum;
 
 import java.io.File;
@@ -8,6 +10,38 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ClassLoaderUtil {
+    public static <T> T getPluginInstance(Class<T> type){
+        EnvEnum env = EnvironmentUtil.getEnv();
+        T t = ClassLoaderUtil.getPluginInstanceByEnvironment(type, env);
+        return t;
+    }
+
+    public static <T> T getPluginInstanceByEnvironment(Class<T> type, EnvEnum envEnum){
+        SelfDefinedClassLoader loader = ClassLoaderFactory.getSelfDefinedClassLoader(envEnum);
+        Class<?> aClass = null;
+        Object newInstance = null;
+        try {
+            aClass = loader.loadClass(type.getName());
+            newInstance = aClass.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        T cast = type.cast(newInstance);
+        return cast;
+    }
+
+
+
+
+    public static SelfDefinedClassLoader getSelfDefinedClassLoader(){
+        EnvEnum env = EnvironmentUtil.getEnv();
+        SelfDefinedClassLoader loader = ClassLoaderFactory.getSelfDefinedClassLoader(env);
+        return loader;
+    }
 
     public static String getRelativePath(EnvEnum envEnum){
 //        System.out.println("getRelativePath");
